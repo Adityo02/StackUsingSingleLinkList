32205831480bded6857d4a9feb07ae98fef816fc
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-11-28 16:53:36 +0700

    add case 3

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -84,6 +84,14 @@ namespace StackUsingSingleLinkList
                         int num = Convert.ToInt32(Console.ReadLine());
                         s.push(num);
                         break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
                 }
             }
         }
